183217279a93967aea7f22d73094fa3e28b3afb5
harryb189 <harryedwardbrown@outlook.com>
2025-11-12 13:36:48 +0000

    isValid function

--- portfolio/wordle/src/main/kotlin/Wordle.kt
+++ portfolio/wordle/src/main/kotlin/Wordle.kt
@@ -1,10 +1,14 @@
-// Implement the six required functions here
 fun isValid(word: String): Boolean {
-
+    if (word.length == 5) {
+        return true
+    }
+    else {
+        return false
+    }
 }
 
 fun readWordList(filename: String): MutableList<String> {
-
+    
 }
 
 fun pickRandomWord(words: MutableList<String>): String {
@@ -20,5 +24,5 @@ fun evaluateGuess(guess: String, target: String): List<Int> {
 }
 
 fun displayGuess(guess: String, matches: List<Int>) {
-    
+
 }
