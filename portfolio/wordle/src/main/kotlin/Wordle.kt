54b6ff7ebe41dc41cf3c5ecd7e6de909e0af90f3
harryb189 <harryedwardbrown@outlook.com>
2025-11-12 13:15:06 +0000

    set up functions

--- portfolio/wordle/src/main/kotlin/Wordle.kt
+++ portfolio/wordle/src/main/kotlin/Wordle.kt
@@ -1,1 +1,24 @@
 // Implement the six required functions here
+fun isValid(word: String): Boolean {
+
+}
+
+fun readWordList(filename: String): MutableList<String> {
+
+}
+
+fun pickRandomWord(words: MutableList<String>): String {
+
+}
+
+fun obtainGuess(attempt: Int): String {
+
+}
+
+fun evaluateGuess(guess: String, target: String): List<Int> {
+
+}
+
+fun displayGuess(guess: String, matches: List<Int>) {
+    
+}
